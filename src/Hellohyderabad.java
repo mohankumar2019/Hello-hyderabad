910c29fbcd6de729314715d935e6854233690359
mohan <mohan.chanti4@gmail.com>
2019-06-08 09:20:55 +0530

    modified file

--- src/Hellohyderabad.java
+++ src/Hellohyderabad.java
@@ -4,8 +4,6 @@ public class Hellohyderabad {
 	public static void main(String[] args) {
 
 		System.out.println("Hello hyderabad123456789tttttttttttttttt");
-		System.out.println("Hello hyderabad123456789ttttttttttttttttttttttttt");
-
 	}
 
 }
